add138586b1683a324311c6ecd0b0a61ad021c7a
benjimenjy <143195267+benjimenjy@users.noreply.github.com>
2024-06-03 09:02:59 -0300

    srgarg

---
+++ Assets/vehiculosmanager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vehiculosmanager : MonoBehaviour
+{
+    public Vehiculo vehiculosScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int cantRuedasVehiculo = vehiculosScript.cantRuedas;
+        Debug.Log("cant ruedas:" + vehiculosScript.cantRuedas);
+        if (cantRuedasVehiculo < 6)
+        {
+            vehiculosScript.gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
